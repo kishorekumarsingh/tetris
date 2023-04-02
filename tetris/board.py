d91ba4f6ce07100224567ee6d6351e3d035f7f43
Kishore <kishore@kwizkosh.com>
2023-04-02 13:16:20 +0530

    Defined board attribute and methods

--- tetris/board.py
+++ tetris/board.py
@@ -1,3 +1,29 @@
 """
 board.py contains the logic related to board
 """
+
+# Immutable board_size tuple
+BOARD_SIZE = (12, 12)
+
+
+class Board:
+    def __init__(self):
+        self.board = [[' ' for _ in range(BOARD_SIZE[1])] for _ in range(BOARD_SIZE[0])]
+
+    def is_valid_move(self, piece, pos):
+        """
+        A valid move is defined thus: if the piece, drawn at its new location,
+        is not outside the bounds of the board, and does not overlap any pieces
+        that previously fell, then the move is valid.
+        :param piece:
+        :param pos:
+        :return:
+        """
+
+    def update_board(self, piece, pos):
+        """
+        Update the board based on the piece and the new position
+        :param piece:
+        :param pos:
+        :return:
+        """
