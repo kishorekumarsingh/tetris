637ffb4d11c4ddd07116053ddf3d6a43394cec6f
Kishore <kishore@kwizkosh.com>
2023-04-02 13:00:37 +0530

    added rotation placeholder code

--- tetris/piece.py
+++ tetris/piece.py
@@ -1,3 +1,22 @@
 """
 piece.py contains the logic related to the pieces
 """
+
+
+class Piece:
+    def __init__(self, piece_str):
+        self.piece = [list(row) for row in piece_str.split('\n')]
+
+    def rotate(self, rotation):
+        # Input move is <a> or <d> : do not rotate
+        if rotation == 0:
+            return self.piece
+
+        # Input move is <s> : rotate clockwise
+        if rotation > 0:
+            # TODO
+            return self.piece
+
+        # Input move is <w>
+        # TODO
+        return self.piece
